f6c403439a99444ce5010b38121c87d38d6370fa
BogdyHd <bogdan.petruta.hd@gmail.com>
2019-06-16 00:37:08 +0300

    major changes

---
+++ Assets/MyGame/Scripts/EndGame.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndGame : MonoBehaviour
+{
+    public Text Score;
+    protected float lastScore;
+    float rResult = 0;
+    public GameObject gameOverImage;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOverImage = GameObject.FindGameObjectWithTag("gameOverImage");
+        gameOverImage.SetActive(false);
+        Score.text = "0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Time.timeScale != 0)
+        {
+            lastScore = Time.time;
+            var result = (Mathf.Round(lastScore * 100)) / 100;
+            Score.text = result.ToString();
+            rResult = result;
+        }
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        gameOverImage.SetActive(true);
+        Score.text = rResult.ToString();
+        Time.timeScale = 0;
+    }
+}
---
+++ Assets/MyGame/Scripts/startFirstLevel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class startFirstLevel : MonoBehaviour
+{
+    public Button Play;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Play.onClick.AddListener(startFLvl);
+    }
+    void startFLvl()
+    {
+        SceneManager.LoadScene("FirstLevel");
+        Time.timeScale = 1;
+    }
+}
